81a36d473ee0d88874aaf6f8b9a99201055f0411
Chatkul Rattanarithikul <billchartkul@hotmail.com>
2023-02-19 19:31:44 +0700

    add: react route dom

--- cv-brill/src/App.tsx
+++ cv-brill/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter} from 'react-router-dom';
 import './App.css';
+import Header from './components/header';
+import Contact from './pages/Contact/contact';
+import Home from './pages/Home/home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="contact" element={<Contact></Contact>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
